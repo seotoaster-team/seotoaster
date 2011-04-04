2875964f4f9cd043819c64b71c6908ae904b5084
Eugene NE NEzhuta <theneiam@gmail.com>
2011-04-04 15:56:30 +0300

    Link helper modified to work with jquery ui. View scripts modifications

--- core/application/views/helpers/ToasterLink.php
+++ core/application/views/helpers/ToasterLink.php
@@ -2,23 +2,12 @@
 
 class Zend_View_Helper_ToasterLink extends Zend_View_Helper_Abstract {
 
-	public function toasterLink($controller, $action, $linkText, $useThinkBox = array(), $hrefOnly = false) {
+	public function toasterLink($controller, $action, $linkText, $params = '', $hrefOnly = false) {
 		$websiteData = Zend_Registry::get('website');
-		$tbTail      = '?TB_iframe=true&height=%height%&width=%width%';
 		$controller  = (substr($controller, 0, 7) == 'backend') ? 'backend/' . $controller : $controller;
 		$linkText    = $this->view->translate($linkText);
-		$href        = $websiteData['url'] . $controller . '/' . $action;
-		if(is_array($useThinkBox) && !empty($useThinkBox)) {
-			$tbTail = str_replace('%height%', $useThinkBox[0], $tbTail);
-			if(isset($useThinkBox[1])) {
-				$tbTail = str_replace('%width%', $useThinkBox[1], $tbTail);
-			}
-			$href .= $tbTail;
-			$link  = '<a href="javascript:;" onclick="tb_show(\'\', \'' . $href . '\', \'\')" title="' . $linkText . '">' . $linkText . '</a>';
-		}
-		else {
-			$link = '<a href="' . $href . '" title="' . $linkText . '">' . $linkText . '</a>';
-		}
+		$href        = $websiteData['url'] . $controller . '/' . $action . '/' . (($params) ? $params : '');
+		$link = '<a class="tpopup" href="javascript:;" url="' . $href . '" title="' . $linkText . '">' . $linkText . '</a>';
 		if($hrefOnly) {
 			return $href;
 		}
