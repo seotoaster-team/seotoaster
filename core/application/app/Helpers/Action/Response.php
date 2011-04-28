feec90f0644a2f906e069a7c6a2908c8cabdbbbf
Eugene NE NEzhuta <theneiam@gmail.com>
2011-04-28 17:17:44 +0300

    Response helper added

---
+++ core/application/app/Helpers/Action/Response.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Send json ecoded response
+ *
+ * @author SeoToaster Dev Team
+ */
+class Helpers_Action_Response extends Zend_Controller_Action_Helper_Abstract {
+
+	const SUCCESS = 'success';
+
+	const FAIL    = 'fail';
+
+	public function success($body = '') {
+		if(!$body) {
+			$body = self::SUCCESS;
+		}
+		$this->_response($body, 0);
+	}
+
+	public function fail($body = '') {
+		if(!$body) {
+			$body = self::FAIL;
+		}
+		$this->_response($body, 1);
+	}
+
+	public function notFound($body = '') {
+		if(!$body) {
+			$body = 'Ooops. Cannot find this page.';
+		}
+		$headers = array(
+			'HTTP/1.1' => '404 Not Found',
+			'Status'   => '404 File not found'
+		);
+		$this->_response($body, 0, 404, $headers);
+	}
+
+	public function response($body, $error = 0, $code = 200, $headers = '') {
+		$this->_response($body, $error, $code, $headers);
+	}
+
+	private function _response($body, $error = 0, $code = 200, $headers = '') {
+		$responseData = json_encode(array(
+			'error'        => $error,
+			'responseText' => $body,
+			'httpCode'     => $code
+		));
+
+		$response = $this->getResponse();
+		$response->setHttpResponseCode($code)
+			->setBody($body);
+
+		if($headers) {
+			if(is_array($headers) && !empty ($headers)){
+				foreach ($headers as $key => $val) {
+					$response->setHeader($key, $val);
+				}
+			}
+			else {
+				$response->setRawHeader($headers);
+			}
+		}
+
+		$response->sendResponse();
+		exit;
+	}
+
+}
+
