a786c61476cb85ac33a9425774db9db41947cc22
Eugene NE NEzhuta <theneiam@gmail.com>
2011-05-13 19:20:27 +0300

    Added 301 redirects handling.

---
+++ core/application/models/DbTable/Redirect.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Redirect database table
+ *
+ * @author Eugene I. Nezhuta [Seotoaster Dev Team] <eugene@seotoaster.com>
+ */
+class Application_Model_DbTable_Redirect extends Zend_Db_Table_Abstract {
+
+	protected $_name           = 'redirect';
+
+}
+
