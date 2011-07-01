f4bec29ee04f48a293f41966d82dcf46e05dd079
Eugene NE NEzhuta <theneiam@gmail.com>
2011-07-01 17:10:57 +0300

    small changes

--- seotoaster_core/application/models/DbTable/Page.php
+++ seotoaster_core/application/models/DbTable/Page.php
@@ -4,11 +4,6 @@ class Application_Model_DbTable_Page extends Zend_Db_Table_Abstract {
 
 	protected $_name = 'page';
 
-	/*protected $_dependentTables = array(
-		'Application_Model_DbTable_Redirect',
-		'Application_Model_DbTable_Container'
-	);*/
-
 	protected $_referenceMap = array(
 		'Template' => array(
 			'columns'       => 'template_id',
