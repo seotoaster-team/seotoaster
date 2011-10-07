1d6a6f720793d5887afa453a3c78d06b4089d7a4
Eugene NE NEzhuta <theneiam@gmail.com>
2011-10-07 18:07:30 +0300

    + Added cache garbage collector

---
+++ seotoaster_core/application/app/Tools/Cache/GarbageCollector.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Centralized garbage collector for cache system
+ *
+ * @author Eugene I. Nezhuta [Seotoaster Dev Team] <eugene@seotoaster.com>
+ */
+class Tools_Cace_GarbageCollector extends Tools_System_GarbageCollector {
+
+	public function clean() {}
+
+	protected function _runOnDefault() {}
+	
+	protected function _runOnCreate() {}
+	protected function _runOnUpdate() {}
+	protected function _runOnDelete() {}
+}
+
