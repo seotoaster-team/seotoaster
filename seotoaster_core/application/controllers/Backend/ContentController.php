a5376f1433fb69e2ff1f5ed4a041f6b706fff205
Eugene NE NEzhuta <theneiam@gmail.com>
2011-10-21 21:11:20 +0300

    + Right panel preview images are changed from the product

--- seotoaster_core/application/controllers/Backend/ContentController.php
+++ seotoaster_core/application/controllers/Backend/ContentController.php
@@ -231,7 +231,7 @@ class Backend_ContentController extends Zend_Controller_Action {
 				$imageSize      = getimagesize($path . '/' . $type . '/' . $image);
 				$imageElement   = htmlspecialchars('<a class="_lbox" href="' . $srcPath . '/' .  self::IMG_CONTENTTYPE_ORIGINAL . '/' . $image . '" title="' . str_replace('-', '&nbsp;', $imageName) . '" class="tpopup"><img border="0" alt="'. str_replace('-', '&nbsp;', $imageName) . '" src="' . $srcPath . '/' . $type . '/' . $image . '" width="' . $imageSize[0] . '" height="' . $imageSize[1] . '" /></a>');
 				$imagesContent .= '<a href="javascript:;" onmousedown="$(\'#content\').tinymce().execCommand(\'mceInsertContent\', false, \'' . $imageElement . '\');">';
-				$imagesContent .= '<img title="' . $image . '" style="vertical-align:top; margin: 0px 0px 4px 4px;" border="0" width="65" src="' . $srcPath . '/'. $type . '/' . $image .'" /></a>';
+				$imagesContent .= '<img title="' . $image . '" style="vertical-align:top; margin: 0px 0px 4px 4px;" border="0" width="65" src="' . $srcPath . '/product/' . $image .'" /></a>';
 			}
 			return $imagesContent;
 		}
