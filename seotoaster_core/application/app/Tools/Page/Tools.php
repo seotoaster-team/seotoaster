3ea1b7be58016ace157ee6ce7bcd71a28f6613e2
Eugene NE NEzhuta <theneiam@gmail.com>
2012-02-07 17:34:28 +0200

    - added mediaServers to all of the widgets

--- seotoaster_core/application/app/Tools/Page/Tools.php
+++ seotoaster_core/application/app/Tools/Page/Tools.php
@@ -9,20 +9,22 @@ class Tools_Page_Tools {
 
 	public static function getPreviewPath($pageId, $capIfNoPreview = false, $croped = false) {
 		$websiteHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('website');
+		$configHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('config');
 		$pageHelper    = Zend_Controller_Action_HelperBroker::getStaticHelper('page');
 		$previews      = Tools_Filesystem_Tools::findFilesByExtension($websiteHelper->getPath() . (($croped) ? $websiteHelper->getPreviewcrop() :$websiteHelper->getPreview()), 'jpg|png|jpeg|gif', true, true, false);
 
 		$page          = Application_Model_Mappers_PageMapper::getInstance()->find($pageId);
+		$websiteUrl    = ($configHelper->getConfig('mediaServers') ? Tools_Content_Tools::applyMediaServers($websiteHelper->getUrl()) : $websiteHelper->getUr());
 		if($page instanceof Application_Model_Models_Page) {
 			$cleanUrl = $pageHelper->clean($page->getUrl());
 			unset($page);
-			$path = (array_key_exists($cleanUrl, $previews)) ? str_replace($websiteHelper->getPath(), $websiteHelper->getUrl(), $previews[$cleanUrl]) : '';
+			$path = (array_key_exists($cleanUrl, $previews)) ? str_replace($websiteHelper->getPath(), $websiteUrl, $previews[$cleanUrl]) : '';
 			if(!$path && $capIfNoPreview) {
-				return $websiteHelper->getUrl() . 'system/images/noimage.png';
+				return $websiteUrl . 'system/images/noimage.png';
 			}
 			return $path;
 		}
-		$websiteHelper->getUrl() . 'system/images/noimage.png';
+		return $websiteUrl . 'system/images/noimage.png';
 	}
 
 	public static function getDraftPages() {
