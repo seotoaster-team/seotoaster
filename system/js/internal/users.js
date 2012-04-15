ff9c883b090dbaf646fed8d7f8c73157eaab8422
Eugene NE NEzhuta <theneiam@gmail.com>
2012-04-15 22:22:27 +0300

    - users list bug fixed

--- system/js/internal/users.js
+++ system/js/internal/users.js
@@ -1,23 +1,20 @@
 $(function() {
 	userCallback();
-    $('#export-users').click(function() {
+	$('#export-users').click(function() {
         $('#expusrs').submit();
     })
 });
 
 function userCallback() {
-	showSpinner();
 	$.getJSON($('#website_url').val() + 'backend/backend_user/list/', function(response) {
 		hideSpinner();
-		$('.sortable').dataTable({
-			"bPaginate"     : false,
-			"bLengthChange" : true,
-			"bInfo"         : false,
-			"bAutoWidth"    : false,
-			"bDestroy"      : true,
-			"bRetrive"      : true,
-			"bProcessing"   : false
-		})
 		$('#users-list tbody').html(response.usersList);
+		var tblContainer = $('.sortable');
+		if(!tblContainer.hasClass('dataTable')) {
+			tblContainer.dataTable({
+				"bInfo"         : false,
+				"bPaginate"     : false
+			});
+		}
 	})
 }
