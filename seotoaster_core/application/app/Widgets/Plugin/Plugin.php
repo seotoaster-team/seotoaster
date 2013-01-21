37760f42d111284175715d60ec96d3d973712b9d
Pavel Kovalyov <sxe.funky@gmail.com>
2013-01-21 17:08:39 +0200

    fixed: whitespaces in links for plugin error messages

--- seotoaster_core/application/app/Widgets/Plugin/Plugin.php
+++ seotoaster_core/application/app/Widgets/Plugin/Plugin.php
@@ -12,8 +12,7 @@ class Widgets_Plugin_Plugin extends Widgets_Abstract {
 		$plugin       = Application_Model_Mappers_PluginMapper::getInstance()->findByName($pluginName);
 		if($plugin !== null) {
 			if($plugin->getStatus() != Application_Model_Models_Plugin::ENABLED) {
-				return $this->_translator->translate('You need install the ') . $plugin->getName() . $this->_translator->translate(' plug-in to view and use this great feature.'). ' '.'<a href="http://www.seotoaster.com/website-plugins-marketplace.html" target="_blank">'.$this->_translator->translate('Download plug-ins here').' '.'</a>'.$this->_translator->translate('and watch a short video to learn how to install plug-ins on your website'). '<a href="http://www.seotoaster.com/how-to-add-a-plugin.html" target="_blank">'.' '.$this->_translator->translate('here').'</a>.';
-        
+				return $this->_translator->translate('You need install the ') . $plugin->getName() . $this->_translator->translate(' plug-in to view and use this great feature.'). ' <a href="http://www.seotoaster.com/website-plugins-marketplace.html" target="_blank">'.$this->_translator->translate('Download plug-ins here').' '.'</a>'.$this->_translator->translate('and watch a short video to learn how to install plug-ins on your website'). ' <a href="http://www.seotoaster.com/how-to-add-a-plugin.html" target="_blank">'.$this->_translator->translate('here').'</a>.';
 			}
 			try {
 				$toasterPlugin = Tools_Factory_PluginFactory::createPlugin($plugin->getName(), $this->_options, $this->_toasterOptions);
@@ -33,7 +32,7 @@ class Widgets_Plugin_Plugin extends Widgets_Abstract {
 			}
 		}
         if(Tools_Security_Acl::isAllowed(Tools_Security_Acl::RESOURCE_ADMINPANEL)) {
-            return $this->_translator->translate('You need the') . ' '. $pluginName . ' '. $this->_translator->translate('plug-in to view and use this great feature.'). '<a href="http://www.seotoaster.com/website-plugins-marketplace.html" target="_blank">'.' '.$this->_translator->translate('Download plug-ins here').' '.'</a>'. $this->_translator->translate('and watch a short video to learn how to install plug-ins on your website'). '<a href="http://www.seotoaster.com/how-to-add-a-plugin.html" target="_blank">'.' '.$this->_translator->translate('here').'</a>.';
+            return $this->_translator->translate('You need the') . ' '. $pluginName . ' '. $this->_translator->translate('plug-in to view and use this great feature.'). ' <a href="http://www.seotoaster.com/website-plugins-marketplace.html" target="_blank">'.$this->_translator->translate('Download plug-ins here').' '.'</a>'. $this->_translator->translate('and watch a short video to learn how to install plug-ins on your website'). ' <a href="http://www.seotoaster.com/how-to-add-a-plugin.html" target="_blank">'.$this->_translator->translate('here').'</a>.';
         }
 		return '';
 	}
