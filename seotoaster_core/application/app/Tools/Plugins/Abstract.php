f9b5838d7d704109086a55edc8ee69e10cc672d3
Pavel Kovalyov <sxe.funky@gmail.com>
2013-04-19 19:32:40 +0300

    fixed: f_cking spaces before <?php tag that was breaking content output 8(

--- seotoaster_core/application/app/Tools/Plugins/Abstract.php
+++ seotoaster_core/application/app/Tools/Plugins/Abstract.php
@@ -1,10 +1,10 @@
-    <?php
+<?php
 
 /**
  * Abstract plugin class. Contains all init parameters,
  *
  * Options, Dispatchers, Optionmakers, etc...
- * @property $emailTriggers array|null List of email triggers-watchers pairs
+ * @property $emailTriggers   array|null List of email triggers-watchers pairs
  * @property $emailRecipients array|null List of plugin specific email recivers
  */
 
@@ -13,24 +13,24 @@ abstract class Tools_Plugins_Abstract implements Interfaces_Plugin {
 	/**
 	 * @const   ACTION_POSTFIX  Method which name ends with this postfix will be dispatched as action
 	 */
-	const ACTION_POSTFIX        = 'Action';
+	const ACTION_POSTFIX = 'Action';
 
 	/**
 	 * @const   OPTION_MAKER_PREFIX Method which name begins with this prefix will be dispatched as content generator
 	 */
-	const OPTION_MAKER_PREFIX   = '_makeOption';
+	const OPTION_MAKER_PREFIX = '_makeOption';
 
-    /**
-     * Name of the placeholder in toaster layout where plugins can inject own content
-     */
-    const INJECT_PLACEHOLDER    = 'plugins';
+	/**
+	 * Name of the placeholder in toaster layout where plugins can inject own content
+	 */
+	const INJECT_PLACEHOLDER = 'plugins';
 
 	/**
 	 * Options for plugin
 	 *
 	 * @var array
 	 */
-	protected $_options         = array();
+	protected $_options = array();
 
 	/**
 	 * All data received from Seotoaster.
@@ -38,14 +38,14 @@ abstract class Tools_Plugins_Abstract implements Interfaces_Plugin {
 	 * Contains at least website url.
 	 * @var array
 	 */
-	protected $_seotoasterData  = array();
+	protected $_seotoasterData = array();
 
 	/**
 	 * Seotoaster session helper
 	 *
 	 * @var Helpers_Action_Session
 	 */
-	protected $_sessionHelper   = null;
+	protected $_sessionHelper = null;
 
 	/**
 	 * Plugin view.
@@ -54,49 +54,49 @@ abstract class Tools_Plugins_Abstract implements Interfaces_Plugin {
 	 * directory is set to plugin_directory/views/
 	 * @var Zend_View
 	 */
-	protected $_view            = null;
+	protected $_view = null;
 
 	/**
 	 * Seotoaster website url
 	 *
 	 * @var string
 	 */
-	protected $_websiteUrl      = null;
+	protected $_websiteUrl = null;
 
 	/**
 	 * Request object
 	 *
 	 * @var Zend_Controller_Request_Http
 	 */
-	protected $_request         = null;
+	protected $_request = null;
 
 	/**
 	 * Response object
 	 *
 	 * @var Zend_Controller_Response_Http
 	 */
-	protected $_response        = null;
+	protected $_response = null;
 
 	/**
 	 * Redirector helper
 	 *
 	 * @var Zend_Controller_Action_Helper_Redirector
 	 */
-	protected $_redirector      = null;
+	protected $_redirector = null;
 
 	/**
 	 * Translator
 	 *
 	 * @var Zend_Translate
 	 */
-	protected $_translator      = null;
+	protected $_translator = null;
 
 	/**
 	 * Path to plugin's languages files
 	 *
 	 * @var string
 	 */
-	protected $_languagesPath   = 'system/languages/';
+	protected $_languagesPath = 'system/languages/';
 
 	/**
 	 * Parameters that has been passed to the plugin
@@ -111,45 +111,45 @@ abstract class Tools_Plugins_Abstract implements Interfaces_Plugin {
 	 *
 	 * @var Helpers_Action_Response
 	 */
-	protected $_responseHelper  = null;
+	protected $_responseHelper = null;
 
-    /**
-     * Access control list binding
-     *
-     * @var array List of ROLE => actions pairs
-     */
-    protected $_securedActions = array();
+	/**
+	 * Access control list binding
+	 *
+	 * @var array List of ROLE => actions pairs
+	 */
+	protected $_securedActions = array();
 
 	/**
 	 * Website helper
 	 *
 	 * @var null|Helpers_Action_Website
 	 */
-	protected $_websiteHelper  = null;
+	protected $_websiteHelper = null;
 
-	protected $_pluginName     = '';
+	protected $_pluginName = '';
 
 	public function  __construct($options, $seotoasterData) {
 		// setting up Seotoaster data and plugin options
-		$this->_options          = $options;
-		$this->_seotoasterData   = $seotoasterData;
+		$this->_options = $options;
+		$this->_seotoasterData = $seotoasterData;
 
 		// setting up helpers
-		$this->_sessionHelper    = Zend_Controller_Action_HelperBroker::getStaticHelper('session');
-		$this->_websiteHelper    = Zend_Controller_Action_HelperBroker::getStaticHelper('website');
-		$this->_responseHelper   = Zend_Controller_Action_HelperBroker::getStaticHelper('response');
-		$this->_redirector       = new Zend_Controller_Action_Helper_Redirector();
+		$this->_sessionHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('session');
+		$this->_websiteHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('website');
+		$this->_responseHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('response');
+		$this->_redirector = new Zend_Controller_Action_Helper_Redirector();
 
 		// setting up request and response objects
 		$front = Zend_Controller_Front::getInstance();
-		$this->_request          = $front->getRequest();
-		$this->_response         = $front->getResponse();
+		$this->_request = $front->getRequest();
+		$this->_response = $front->getResponse();
 		unset($front);
 
 		// setting up view
-		$this->_view             = new Zend_View();
-		$this->_websiteUrl       = $this->_websiteHelper->getUrl();
-		$this->_pluginName       = strtolower(__CLASS__);
+		$this->_view = new Zend_View();
+		$this->_websiteUrl = $this->_websiteHelper->getUrl();
+		$this->_pluginName = strtolower(__CLASS__);
 		$this->_view->websiteUrl = $this->_websiteUrl;
 		$this->_view->pluginName = $this->_pluginName;
 
@@ -170,11 +170,11 @@ abstract class Tools_Plugins_Abstract implements Interfaces_Plugin {
 	protected function _initTranslator() {
 		$websiteHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('website');
 		$this->_translator = Zend_Registry::get('Zend_Translate');
-		$langsPath         = $websiteHelper->getPath() . 'plugins/' . strtolower(get_called_class()) . '/' . $this->_languagesPath;
-		if(is_dir($langsPath) && is_readable($langsPath)) {
+		$langsPath = $websiteHelper->getPath() . 'plugins/' . strtolower(get_called_class()) . '/' . $this->_languagesPath;
+		if (is_dir($langsPath) && is_readable($langsPath)) {
 			$locale = Zend_Registry::get('Zend_Locale');
-			if(!file_exists($langsPath . $locale->getLanguage() . '.lng')) {
-				if(Tools_System_Tools::debugMode()) {
+			if (!file_exists($langsPath . $locale->getLanguage() . '.lng')) {
+				if (Tools_System_Tools::debugMode()) {
 					error_log('Language file ' . $locale->getLanguage() . '.lng does not exist');
 				}
 				return false;
@@ -183,51 +183,51 @@ abstract class Tools_Plugins_Abstract implements Interfaces_Plugin {
 				$this->_translator->addTranslation(array(
 					'content' => $langsPath . $locale->getLanguage() . '.lng',
 					'locale'  => $locale,
-			        'clear'   => true
+					'clear'   => true
 				));
 			} catch (Exception $e) {
-				if(Tools_System_Tools::debugMode()) {
+				if (Tools_System_Tools::debugMode()) {
 					error_log("(plugin: " . strtolower(get_called_class()) . ") " . $e->getMessage() . "\n" . $e->getTraceAsString());
 				}
 			}
 		}
 	}
 
-    /**
-     * Access control list initializing
-     */
-    protected function _initAcl() {
-        if (!empty($this->_securedActions)){
-            $acl = Zend_Registry::get('acl');
-            foreach($this->_securedActions as $role => $actions){
-                if (is_array($actions) && !empty($actions)){
-                    foreach ($actions as $action){
-                        $resource = new Zend_Acl_Resource(get_called_class().'-'.$action);
-                        if (! $acl->has($resource)){
-                            $acl->addResource($resource);
-                        }
-                        $acl->allow($role, $resource);
-                    }
-                }
-            }
-            Zend_Registry::set('acl', $acl);
-        }
-    }
+	/**
+	 * Access control list initializing
+	 */
+	protected function _initAcl() {
+		if (!empty($this->_securedActions)) {
+			$acl = Zend_Registry::get('acl');
+			foreach ($this->_securedActions as $role => $actions) {
+				if (is_array($actions) && !empty($actions)) {
+					foreach ($actions as $action) {
+						$resource = new Zend_Acl_Resource(get_called_class() . '-' . $action);
+						if (!$acl->has($resource)) {
+							$acl->addResource($resource);
+						}
+						$acl->allow($role, $resource);
+					}
+				}
+			}
+			Zend_Registry::set('acl', $acl);
+		}
+	}
 
 	public function run($requestedParams = array()) {
 		$this->_requestedParams = $requestedParams;
-		$optionResult           = $this->_dispatchOptions();
-		if($optionResult) {
+		$optionResult = $this->_dispatchOptions();
+		if ($optionResult) {
 			return $optionResult;
 		}
 		$this->_dispatch($requestedParams);
 	}
 
 	protected function _dispatchOptions() {
-		if(!empty($this->_options)) {
+		if (!empty($this->_options)) {
 			foreach ($this->_options as $option) {
 				$optionMakerName = self::OPTION_MAKER_PREFIX . ucfirst($option);
-				if(in_array($optionMakerName, get_class_methods($this))) {
+				if (in_array($optionMakerName, get_class_methods($this))) {
 					return $this->$optionMakerName();
 				}
 				//return '<strong>Invalid option</strong>';
@@ -236,20 +236,21 @@ abstract class Tools_Plugins_Abstract implements Interfaces_Plugin {
 		}
 	}
 
-    /**
-     * Inject plugin's content into the toaster layout
-     *
-     * @param string $content
-     */
-    protected function _injectContent($content) {
-        Zend_Layout::getMvcInstance()->getView()->placeholder(self::INJECT_PLACEHOLDER)->append($content);
-    }
+	/**
+	 * Inject plugin's content into the toaster layout
+	 *
+	 * @param string $content
+	 */
+	protected function _injectContent($content) {
+		Zend_Layout::getMvcInstance()->getView()->placeholder(self::INJECT_PLACEHOLDER)->append($content);
+	}
 
 	private function _dispatch($params) {
 		$action = (isset($params['run'])) ? $params['run'] . self::ACTION_POSTFIX : null;
-		if($action !== null) {
-			if(in_array($action, get_class_methods($this))
-                    && Tools_Security_Acl::isAllowed(get_called_class().'-'.$params['run'])) {
+		if ($action !== null) {
+			if (in_array($action, get_class_methods($this))
+					&& Tools_Security_Acl::isAllowed(get_called_class() . '-' . $params['run'])
+			) {
 				$this->$action();
 				exit;
 			}
