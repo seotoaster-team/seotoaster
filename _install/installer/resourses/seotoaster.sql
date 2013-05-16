e3a510567b9406cbbebb96385c4c5a1fdb8dd965
Pavel Kovalyov <sxe.funky@gmail.com>
2013-05-16 19:23:29 +0300

    fixed: missing coma

--- _install/installer/resourses/seotoaster.sql
+++ _install/installer/resourses/seotoaster.sql
@@ -331,7 +331,7 @@ CREATE TABLE IF NOT EXISTS `template_type` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `template_type` (`id`, `title`) VALUES
-('typeregular', 'Regular page')
+('typeregular', 'Regular page'),
 ('typemail', 'E-mail sending'),
 ('typemobile', 'Mobile page');
 
