fe86dce3496932023864cd30b07ce5d3518e0117
zhenya111 <zhenyaaynehz@gmail.com>
2013-06-13 19:29:48 +0300

    - Added option :readonly for content holding widgets

--- seotoaster_core/application/app/Widgets/AbstractContent.php
+++ seotoaster_core/application/app/Widgets/AbstractContent.php
@@ -16,34 +16,37 @@ abstract class Widgets_AbstractContent extends Widgets_Abstract {
 	protected function _addAdminLink($containerType, $containerId = null, $title = '', $width = 0, $height = 0) {
 		$adminIconName = '';
 		$imgAlt        = '';
-		switch ($containerType) {
-			case Application_Model_Models_Container::TYPE_REGULARCONTENT:
-				$adminIconName = 'editadd.png';
-				$imgAlt        = 'edit content';
-			break;
-			case Application_Model_Models_Container::TYPE_STATICCONTENT:
-				$adminIconName = 'editadd-static-content.png';
-				$imgAlt        = 'edit static content';
-			break;
-			case Application_Model_Models_Container::TYPE_REGULARHEADER:
-				$adminIconName = 'editadd-header.png';
-				$imgAlt        = 'edit header';
-			break;
-			case Application_Model_Models_Container::TYPE_STATICHEADER:
-				$adminIconName = 'editadd-static-header.png';
-				$imgAlt        = 'edit static header';
-			break;
-			case Application_Model_Models_Container::TYPE_CODE:
-				$adminIconName = 'editadd-code.png';
-				$imgAlt        = 'edit code';
-			break;
-		}
-		if(null == $containerId) {
-			return '<a class="tpopup generator-links" data-pwidth="' . $width . '" data-pheight="' . $height . '" title="Click to ' . $imgAlt . '" href="javascript:;" data-url="' . $this->_toasterOptions['websiteUrl'] . 'backend/backend_content/add/containerType/' . $containerType . '/containerName/' . $this->_options[0] . '/pageId/' . $this->_toasterOptions['id'] . '" class="generator-links"><img width="26" height="26" src="' . $this->_toasterOptions['websiteUrl'] . 'system/images/' . $adminIconName . '" alt="'. $imgAlt .'" /></a>';
-		}
 
-		return '<a class="tpopup generator-links" data-pwidth="' . $width . '" data-pheight="' . $height . '" title="Click to ' . $imgAlt . '" href="javascript:;" data-url="'. $this->_toasterOptions['websiteUrl'] . 'backend/backend_content/edit/id/' . $containerId . '/containerType/' . $containerType . '"  class="generator-links"><img width="26" height="26" src="' . $this->_toasterOptions['websiteUrl'] . 'system/images/' . $adminIconName .'" alt="'. $imgAlt .'" /></a>';
-	}
+        if (!in_array('readonly', $this->_options)) {
+            switch ($containerType) {
+                case Application_Model_Models_Container::TYPE_REGULARCONTENT:
+                    $adminIconName = 'editadd.png';
+                    $imgAlt        = 'edit content';
+                break;
+                case Application_Model_Models_Container::TYPE_STATICCONTENT:
+                    $adminIconName = 'editadd-static-content.png';
+                    $imgAlt        = 'edit static content';
+                break;
+                case Application_Model_Models_Container::TYPE_REGULARHEADER:
+                    $adminIconName = 'editadd-header.png';
+                    $imgAlt        = 'edit header';
+                break;
+                case Application_Model_Models_Container::TYPE_STATICHEADER:
+                    $adminIconName = 'editadd-static-header.png';
+                    $imgAlt        = 'edit static header';
+                break;
+                case Application_Model_Models_Container::TYPE_CODE:
+                    $adminIconName = 'editadd-code.png';
+                    $imgAlt        = 'edit code';
+                break;
+            }
+            if(null == $containerId) {
+                return '<a class="tpopup generator-links" data-pwidth="' . $width . '" data-pheight="' . $height . '" title="Click to ' . $imgAlt . '" href="javascript:;" data-url="' . $this->_toasterOptions['websiteUrl'] . 'backend/backend_content/add/containerType/' . $containerType . '/containerName/' . $this->_options[0] . '/pageId/' . $this->_toasterOptions['id'] . '" class="generator-links"><img width="26" height="26" src="' . $this->_toasterOptions['websiteUrl'] . 'system/images/' . $adminIconName . '" alt="'. $imgAlt .'" /></a>';
+            }
+
+            return '<a class="tpopup generator-links" data-pwidth="' . $width . '" data-pheight="' . $height . '" title="Click to ' . $imgAlt . '" href="javascript:;" data-url="'. $this->_toasterOptions['websiteUrl'] . 'backend/backend_content/edit/id/' . $containerId . '/containerType/' . $containerType . '"  class="generator-links"><img width="26" height="26" src="' . $this->_toasterOptions['websiteUrl'] . 'system/images/' . $adminIconName .'" alt="'. $imgAlt .'" /></a>';
+        }
+    }
 
 	protected function _init() {
 		parent::_init();
