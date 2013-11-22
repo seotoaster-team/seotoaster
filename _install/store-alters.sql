51ffad75ad47c7b67ccd738b3c608c4a30a750ea
Vitaly-V <gtopbox@gmail.com>
2013-11-22 11:25:11 +0200

    Update store-alters.sql

--- _install/store-alters.sql
+++ _install/store-alters.sql
@@ -1,4 +1,4 @@
--- version: 2.1.1
+-- version: 2.1.0
 
 -- These alters are always the latest and updated version of the database
 UPDATE `shopping_config` SET `value`='2.1.1' WHERE `name`='version';
