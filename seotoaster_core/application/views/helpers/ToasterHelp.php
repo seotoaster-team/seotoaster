d20f113daddf9848d8ee2b04e32f10cf6d7f52b5
Pavel Kovalyov <sxe.funky@gmail.com>
2014-01-16 16:55:08 +0200

    updated: manage draft help link

--- seotoaster_core/application/views/helpers/ToasterHelp.php
+++ seotoaster_core/application/views/helpers/ToasterHelp.php
@@ -5,59 +5,60 @@
  * Date: 5/28/12
  * Time: 6:35 PM
  */
-class Zend_View_Helper_ToasterHelp extends Zend_View_Helper_Abstract {
+class Zend_View_Helper_ToasterHelp extends Zend_View_Helper_Abstract
+{
 
-    const REMOTE_URL           = 'http://www.seotoaster.com/';
+    const REMOTE_URL = 'http://www.seotoaster.com/';
 
-    const SECTION_ADDPAGE      = 'addpage';
+    const SECTION_ADDPAGE = 'addpage';
 
-    const SECTION_EDITPAGE     = 'editpage';
+    const SECTION_EDITPAGE = 'editpage';
 
-    const SECTION_ORGANIZE     = 'organize';
+    const SECTION_ORGANIZE = 'organize';
 
-    const SECTION_DRAFT        = 'draft';
+    const SECTION_DRAFT = 'draft';
 
     const SECTION_UPLOADTHINGS = 'uploadthings';
 
     const SECTION_REMOVETHINGS = 'removethings';
 
-    const SECTION_ROBOTS       = 'robots';
+    const SECTION_ROBOTS = 'robots';
 
-    const SECTION_301S         = '301s';
+    const SECTION_301S = '301s';
 
-    const SECTION_DEEPLINKS    = 'deeplinks';
+    const SECTION_DEEPLINKS = 'deeplinks';
 
-    const SECTION_SCULPTING    = 'sculpting';
+    const SECTION_SCULPTING = 'sculpting';
 
-    const SECTION_ADDTEMPLATE  = 'addtemplate';
+    const SECTION_ADDTEMPLATE = 'addtemplate';
 
     const SECTION_EDITTEMPLATE = 'edittemplate';
 
-    const SECTION_EDITCSS      = 'editcss';
+    const SECTION_EDITCSS = 'editcss';
 
-    const SECTION_THEMES       = 'themes';
+    const SECTION_THEMES = 'themes';
 
-    const SECTION_CONFIG       = 'config';
+    const SECTION_CONFIG = 'config';
 
-    const SECTION_USERS        = 'users';
+    const SECTION_USERS = 'users';
 
-    const SECTION_PLUGINS      = 'plugins';
+    const SECTION_PLUGINS = 'plugins';
 
-    const SECTION_FA           = 'fa';
+    const SECTION_FA = 'fa';
 
-    const SECTION_EDITCONTENT  = 'content';
+    const SECTION_EDITCONTENT = 'content';
 
     const SECTION_ACTIONEMAILS = 'actionemails';
 
-	const SECTION_EDITFORM     = 'editform';
+    const SECTION_EDITFORM = 'editform';
 
-	const SECTION_WIDCARD    = 'widcard';
+    const SECTION_WIDCARD = 'widcard';
 
-    private $_helpHashMap  = array(
+    private $_helpHashMap = array(
         self::SECTION_ADDPAGE      => 'how-to-create-and-edit-a-page.html',
         self::SECTION_EDITPAGE     => 'how-to-create-and-edit-a-page.html',
         self::SECTION_ORGANIZE     => 'how-to-organize-pages.html',
-        self::SECTION_DRAFT        => 'how-to-put-page-in-draft.html',
+        self::SECTION_DRAFT    => 'how-to-set-page-as-draft.html',
         self::SECTION_UPLOADTHINGS => 'how-to-upload-things.html',
         self::SECTION_REMOVETHINGS => 'how-to-remove-things.html',
         self::SECTION_ROBOTS       => 'how-to-edit-robottxt.html',
@@ -74,15 +75,16 @@ class Zend_View_Helper_ToasterHelp extends Zend_View_Helper_Abstract {
         self::SECTION_FA           => 'how-to-add-and-edit-a-featured-area.html',
         self::SECTION_ACTIONEMAILS => 'action-emails-cheat-sheet.html',
         self::SECTION_EDITCONTENT  => 'add-and-edit-content.html',
-	    self::SECTION_EDITFORM     => 'how-to-add-a-form.html',
-	    self::SECTION_WIDCARD      => 'local-search-engine-optimization.html'
+        self::SECTION_EDITFORM => 'how-to-add-a-form.html',
+        self::SECTION_WIDCARD  => 'local-search-engine-optimization.html'
     );
 
-    public function toasterHelp($section, $hashMap = null) {
-        if(is_array($hashMap)){
+    public function toasterHelp($section, $hashMap = null)
+    {
+        if (is_array($hashMap)) {
             $this->_helpHashMap = array_merge($this->_helpHashMap, $hashMap);
         }
-        if(array_key_exists($section, $this->_helpHashMap)) {
+        if (array_key_exists($section, $this->_helpHashMap)) {
             return '<a class="help icon-help btn link icon inverse-info" href="' . self::REMOTE_URL . $this->_helpHashMap[$section] . '" target="_blank"></a>';
         }
         return '<a class="help icon-help btn link icon inverse-info" href="javascript:;"></a>';
