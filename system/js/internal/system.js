217abb96b6337e871c83ca2220a9c6e0a7b1481f
Pavel Kovalyov <sxe.funky@gmail.com>
2014-01-16 18:21:07 +0200

    updated: checkbox wrapper in system.js

--- system/js/internal/system.js
+++ system/js/internal/system.js
@@ -197,12 +197,12 @@ doc.on('click', '[name="useImage"]', function () {
 });
 ///////// checkbox & radio button //////////////
 function checkboxRadio() {
-    $(':checkbox, :radio').not('.processed, .icon, .hidden').each(function () {
-        if(!$(this).closest('.btn-set').length) {
+    $('input:checkbox, input:radio').not('.processed, .icon, .hidden').each(function () {
+        if (!$(this).closest('.btn-set').length) {
             if ($(this).parent('label').length) {
-                !$(this).hasClass('switcher') ? $(this).after('<span></span>') : $(this).after('<span><span></span></span>');
-            }else {
-                !$(this).hasClass('switcher') ? $(this).wrap('<label class="checkbox_radio"></label>').after('<span></span>') : $(this).wrap('<label class="checkbox_radio"></label>').after('<span><span></span></span>');
+                !$(this).hasClass('switcher') ? $(this).after('<span class="checkbox_radio"></span>') : $(this).after('<span class="checkbox_radio"><span></span></span>');
+            } else {
+                !$(this).hasClass('switcher') ? $(this).wrap('<label class="pointer"></label>').after('<span class="checkbox_radio"></span>') : $(this).wrap('<label class="pointer checkbox_radio-wrap"></label>').after('<span class="checkbox_radio"><span></span></span>');
             }
         }
         $(this).addClass('processed');
--- system/js/scripts.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var a, b, c;
-    a = "0 0 10px @gray-dark",
-    b = a.indexOf(' ') - 1;
-
-    return a.replace(a.substring(c, b), '').replace('[', '');
-}())
