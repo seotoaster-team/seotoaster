3c00f248f8cd2ac323b4f369b4b362151e126c26
Vitaly-V <gtopbox@gmail.com>
2014-01-23 13:14:34 +0200

    Update store-alters.sql

--- _install/store-alters.sql
+++ _install/store-alters.sql
@@ -5,6 +5,7 @@ ALTER TABLE `shopping_cart_session` ADD COLUMN `discount_tax_rate` enum('0','1',
 ALTER TABLE `shopping_cart_session` ADD COLUMN `shipping_tax` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT 'Shipping Tax' AFTER `sub_total`;
 ALTER TABLE `shopping_cart_session` ADD COLUMN `discount_tax` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT 'Discount Tax' AFTER `shipping_tax`;
 ALTER TABLE `shopping_cart_session` ADD COLUMN `sub_total_tax` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT 'Sub total Tax' AFTER `discount_tax`;
+ALTER TABLE `shopping_quote` ADD COLUMN `creator_id` int(10) unsigned DEFAULT '0' AFTER `edited_by`;
 
 -- These alters are always the latest and updated version of the database
 UPDATE `shopping_config` SET `value`='2.1.2' WHERE `name`='version';
