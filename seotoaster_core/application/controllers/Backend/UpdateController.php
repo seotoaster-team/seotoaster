2e3302eb13d840fbb73b4c3bf0494648cfecb8b8
vyrodov <vitali@seosamba.com>
2014-01-24 19:02:14 +0200

    Updater draft

--- seotoaster_core/application/controllers/Backend/UpdateController.php
+++ seotoaster_core/application/controllers/Backend/UpdateController.php
@@ -8,14 +8,14 @@
 class Backend_UpdateController extends Zend_Controller_Action
 {
 
-    const MASTER_CMS_LINK = 'http://seotoaster.com/cms.txt';
-    const MASTER_STORE_LINK = 'http://seotoaster.com/store.txt';
+    const MASTER_CMS_LINK       = 'http://seotoaster.com/cms.txt';
+    const MASTER_STORE_LINK     = 'http://seotoaster.com/store.txt';
 
-    protected $_downloadLink = null;
-    protected $_toasterVersion = null;
-    protected $_remoteVersion = null;
-    protected $_websitePath = null;
-    protected $_tmpPath = null;
+    protected $_downloadLink    = null;
+    protected $_toasterVersion  = null;
+    protected $_remoteVersion   = null;
+    protected $_websitePath     = null;
+    protected $_tmpPath         = null;
 
     public function init()
     {
@@ -28,20 +28,18 @@ class Backend_UpdateController extends Zend_Controller_Action
     public function updateAction()
     {
         $this->_websitePath = $this->_helper->website->getPath();
-        $this->_tmpPath = $this->_helper->website->getPath() . $this->_helper->website->getTmp();
+        $this->_tmpPath = $this->_helper->website->getPath() . $this->_helper->website->getTmp() . 'updates';
         try {
             if (file_exists($this->_websitePath . 'plugins/shopping/version.txt')) {
-                $this->_toasterVersion = trim(
-                    Tools_Filesystem_Tools::getFile($this->_websitePath . 'plugins/shopping/version.txt')
-                );
+                $this->_toasterVersion = trim(Tools_Filesystem_Tools::getFile($this->_websitePath . 'plugins/shopping/version.txt'));
                 $master_link = self::MASTER_STORE_LINK;
             } else {
                 $this->_toasterVersion = trim(Tools_Filesystem_Tools::getFile('version.txt'));
                 $master_link = self::MASTER_CMS_LINK;
             }
             $master_versions = explode("\n", file_get_contents($master_link));
-            $this->_remoteVersion = filter_var($master_versions [0], FILTER_SANITIZE_STRING);
-            $this->_downloadLink = filter_var($master_versions [1], FILTER_SANITIZE_URL);
+            $this->_remoteVersion   = filter_var($master_versions [0], FILTER_SANITIZE_STRING);
+            $this->_downloadLink    = filter_var($master_versions [1], FILTER_SANITIZE_URL);
         } catch (Exceptions_SeotoasterException $se) {
             if (self::debugMode()) {
                 error_log($se->getMessage());
@@ -49,9 +47,12 @@ class Backend_UpdateController extends Zend_Controller_Action
         }
         $updateStatus = version_compare($this->_remoteVersion, $this->_toasterVersion);
         if (1 === $updateStatus) {
-            $this->_getZip();
-            $this->_copyConfigs();
-            $this->_copyToaster($this->_tmpPath . 'updates', $this->_websitePath);
+            //$this->_getZip();
+            //$this->_copyFiles();
+            //$this->_copyToaster($this->_tmpPath, $this->_websitePath);
+            //db TODO: delete hardcode
+            $this->_updateDataBase('cms');
+            $this->_redirect($this->_helper->website->getUrl(), array('exit' => true));
         } elseif (-1 === $updateStatus) {
             return 'Your version of the system is higher than the remote';
         } else {
@@ -61,8 +62,8 @@ class Backend_UpdateController extends Zend_Controller_Action
 
     protected function _getZip()
     {
-        $upZip = $this->_tmpPath . 'toaster.zip';
-        $upDir = $this->_tmpPath . 'updates';
+        $upZip = $this->_helper->website->getTmp() . 'toaster.zip';
+        $upDir = $this->_tmpPath;
         mkdir($upDir);
         set_time_limit(0);
         $fp = fopen($upZip, 'w+');
@@ -84,21 +85,14 @@ class Backend_UpdateController extends Zend_Controller_Action
         $filter->filter($upZip);
     }
 
-    protected function _copyConfigs()
+    protected function _copyFiles()
     {
-
-        if (!copy($this->_websitePath . 'system/coreinfo.php', $this->_tmpPath . 'updates/system/coreinfo.php')) {
-            echo "can't copy";
+        if (!copy($this->_websitePath . 'system/coreinfo.php', $this->_tmpPath . DIRECTORY_SEPARATOR . 'system/coreinfo.php')) {
+            return false;
         }
-        $confDir = $this->_websitePath . 'seotoaster_core/application/configs/';
-        $upConfDir = $this->_tmpPath . 'updates/seotoaster_core/application/configs/';
-        $dir = opendir($confDir);
-        while (false !== ($file = readdir($dir))) {
-            if (($file != '.') && ($file != '..')) {
-                copy($confDir . '/' . $file, $upConfDir . '/' . $file);
-            }
-        }
-        closedir($dir);
+        mkdir( $this->_tmpPath . DIRECTORY_SEPARATOR . '_install');
+        $this->_copyToaster($this->_tmpPath . DIRECTORY_SEPARATOR . 'install', $this->_tmpPath . DIRECTORY_SEPARATOR . '_install');
+        return true;
     }
 
     protected function _copyToaster($source, $dest)
@@ -112,8 +106,23 @@ class Backend_UpdateController extends Zend_Controller_Action
                 mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
             } else {
                 copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+                unlink($item->getPathName());
             }
         }
+        Tools_Filesystem_Tools::deleteDir($source);
+        return true;
+    }
 
+    protected function _updateDataBase($type = 'cms')
+    {
+        //TODO: Add more checks.
+        $alters = stristr(trim(Tools_Filesystem_Tools::getFile($this->_websitePath . '_install/alters.sql')), '-- version: ' .  $this->_toasterVersion);
+        $sqlAlters = Tools_System_SqlSplitter::split($alters);
+        $db = new Zend_Db_Table();
+        //$db->getAdapter()->beginTransaction();
+        foreach ($sqlAlters as $alter) {
+            $db->getAdapter()->insert(array($alter));
+        }
+        //$db->getAdapter()->endTransaction();
     }
 }
