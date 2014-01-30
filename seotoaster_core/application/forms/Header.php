53edcd2e5cff392efe4cd3d1d5a0bf726e5d7bd6
Pavel Kovalyov <sxe.funky@gmail.com>
2014-01-30 18:25:01 +0200

    fixed: header editor

--- seotoaster_core/application/forms/Header.php
+++ seotoaster_core/application/forms/Header.php
@@ -1,30 +1,42 @@
 <?php
 
-class Application_Form_Header extends Application_Form_Container {
+class Application_Form_Header extends Application_Form_Container
+{
 
-	public function init() {
-		if(!$this->_containerType) {
-			$this->_containerType = Application_Model_Models_Container::TYPE_REGULARHEADER;
-		}
+    public function init()
+    {
 
-		$this->addAttribs(array(
-			'class' => '_fajax _reload'
-		));
+        $this->addElement(
+            'text',
+            'content',
+            array(
+                'id'      => 'content',
+                'value'   => $this->_content,
+                'filters' => array('StringTrim'),
+                'class'   => 'header-content'
+            )
+        );
 
-		$this->addElement('text', 'content', array(
-			'id'       => 'content',
-			'value'    => $this->_content,
-			'filters'  => array('StringTrim'),
-			'class'    => 'header-content'
-		));
+        $this->addElement(
+            'button',
+            'submit',
+            array(
+                'type'   => 'submit',
+                'id'     => 'btn-submit',
+                'label'  => 'Save content',
+                'class'  => 'formsubmit mt15px',
+                'ignore' => true
+            )
+        );
 
-		parent::init();
+        parent::init();
 
-		$this->addElement('submit', 'submit', array(
-			'id'     => 'btn-submit',
-			'label'  => 'Save content',
-			'class'  => 'formsubmit mt15px',
-			'ignore' => true
-		));
+        if (!$this->_containerType) {
+            $this->_containerType = Application_Model_Models_Container::TYPE_REGULARHEADER;
+        }
+
+        $classes = $this->getAttrib('class');
+        $classes .= ' _fajax _reload';
+        $this->setAttrib('class', $classes);
     }
 }
