bb1dcf3ba0d6f1f53891096eb882cb1503a03ce7
zhenya111 <zhenyaaynehz@gmail.com>
2014-02-04 15:33:49 +0200

    Solving the problem of output message "password reset" for widget "Member".

--- seotoaster_core/application/controllers/LoginController.php
+++ seotoaster_core/application/controllers/LoginController.php
@@ -117,7 +117,7 @@ class LoginController extends Zend_Controller_Action {
 				)));
 				$resetTokenId = Application_Model_Mappers_PasswordRecoveryMapper::getInstance()->save($resetToken);
 				if($resetTokenId) {
-					$this->_helper->flashMessenger->addMessage('We\'ve sent an email to ' . $user->getEmail() . ' containing a temporary url that will allow you to reset your password for the next 24 hours. Please check your spam folder if the email doesn\'t appear within a few minutes.');
+					$this->_helper->flashMessenger->setNamespace('passreset')->addMessage('We\'ve sent an email to '.$user->getEmail().' containing a temporary url that will allow you to reset your password for the next 24 hours. Please check your spam folder if the email doesn\'t appear within a few minutes.');
                     if(isset($this->_helper->session->retrieveRedirect)){
                         $redirectTo = $this->_helper->session->retrieveRedirect;
                         unset($this->_helper->session->retrieveRedirect);
