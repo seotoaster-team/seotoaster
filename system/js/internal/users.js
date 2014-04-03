78f2342e5a0955d3fdbeaeb00e79bdf0cdabe777
Vitaly-V <gtopbox@gmail.com>
2014-04-03 15:28:32 +0300

    Add pagination to user screen

--- system/js/internal/users.js
+++ system/js/internal/users.js
@@ -1,21 +1,6 @@
 $(function() {
-	userCallback();
-	$('#export-users').click(function() {
+    $('#export-users').click(function() {
         $('#expusrs').submit();
     })
 });
-
-function userCallback() {
-	$.getJSON($('#website_url').val() + 'backend/backend_user/list/', function(response) {
-		hideSpinner();
-		$('#users-list tbody').html(response.usersList);
-		var tblContainer = $('.sortable');
-		if(!tblContainer.hasClass('dataTable')) {
-			tblContainer.dataTable({
-				"bInfo"         : false,
-				"bPaginate"     : false
-			});
-		}
-	})
-}
 
