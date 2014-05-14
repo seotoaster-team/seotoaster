772ffc67554be3fea557200b7368ee5b70f691fc
PavelCSS <yashin.ziby@gmail.com>
2014-05-14 18:45:24 +0300

    Change order toolbar in editor

--- system/js/internal/tinymceInit.js
+++ system/js/internal/tinymceInit.js
@@ -16,8 +16,8 @@ $(document).ready(function(){
             "advlist lists link image charmap", "visualblocks code fullscreen", "media table paste importcss textcolor stw"
         ],
         importcss_merge_classes : true,
-        toolbar1                : "stw | styleselect | formatselect | fontsizeselect | "+"pastetext removeformat | visualblocks code fullscreen",
-        toolbar2                : "bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist | "+"forecolor backcolor | link image media table | hr charmap",
+        toolbar1                : "bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist | forecolor backcolor | link image media table | pastetext removeformat",
+        toolbar2                : "stw | styleselect | formatselect | fontsizeselect | hr charmap | visualblocks code fullscreen",
         fontsize_formats        : "8px 10px 12px 14px 18px 24px 36px",
         block_formats           : "Block=div;Paragraph=p;Block Quote=blockquote;Address=address;Code=code;Preformatted=pre;H2=h2;H3=h3;H4=h4;H5=h5;H6=h6",
         link_list               : websiteUrl+'backend/backend_page/linkslist/',
