b1293124209963d701a44808bbce4454daab4e21
PavelCSS <yashin.ziby@gmail.com>
2014-05-27 11:12:59 +0300

    Fix and Update editor

--- system/js/internal/tinymceInit.js
+++ system/js/internal/tinymceInit.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
         importcss_merge_classes : true,
         toolbar1                : "bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist | forecolor backcolor | link image media table | pastetext removeformat",
         toolbar2                : "stw | styleselect | formatselect | fontsizeselect | hr charmap | visualblocks code fullscreen",
-        fontsize_formats        : "8px 10px 12px 14px 18px 24px 36px",
+        fontsize_formats        : "8px 10px 12px 14px 16px 18px 24px 36px",
         block_formats           : "Block=div;Paragraph=p;Block Quote=blockquote;Address=address;Code=code;Preformatted=pre;H2=h2;H3=h3;H4=h4;H5=h5;H6=h6",
         link_list               : websiteUrl+'backend/backend_page/linkslist/',
         image_advtab            : true,
