efe85e065f7a7d1a426d3a50b2bd19b6e41d3781
Artem <kovaloff1@gmail.com>
2014-06-23 16:04:44 +0300

    Fix problem with memberonly rights

--- seotoaster_core/application/app/MagicSpaces/Adminonly/Adminonly.php
+++ seotoaster_core/application/app/MagicSpaces/Adminonly/Adminonly.php
@@ -2,7 +2,7 @@
 class MagicSpaces_Adminonly_Adminonly extends Tools_MagicSpaces_Abstract {
 
 	protected function _run() {
-		return (Tools_Security_Acl::isAllowed(Tools_Security_Acl::RESOURCE_ADMINPANEL)) ? $this->_spaceContent : '';
+		return (Tools_Security_Acl::isAllowed(Tools_Security_Acl::RESOURCE_PLUGINS)) ? $this->_spaceContent : '';
 	}
 
 }
