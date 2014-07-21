4df85653085c32562e0b5c8073cf8ab8172a4da9
zhenya <eugene.lysenko@seosamba.com>
2014-07-21 11:23:52 +0300

    Reformate code

--- seotoaster_core/application/app/Tools/Factory/MagicSpaceFactory.php
+++ seotoaster_core/application/app/Tools/Factory/MagicSpaceFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Magic spaces factory
  *
@@ -6,31 +7,35 @@
  * Date: 1/25/12
  * Time: 3:57 PM
  */
+class Tools_Factory_MagicSpaceFactory
+{
+    private function  __construct()
+    {
 
-class Tools_Factory_MagicSpaceFactory {
+    }
 
-	private function  __construct() {}
+    public static function createMagicSpace($name, $content, $toasterData, $params = array())
+    {
+        $name = ucfirst(strtolower($name));
+        if (self::_validate($name)) {
+            $magicSpaceClassName = 'MagicSpaces_'.$name.'_'.$name;
+            return new $magicSpaceClassName($name, $content, $toasterData, $params);
+        }
+        throw new Exceptions_SeotoasterException('Cannot run the magic space: '.$name);
+    }
 
-	public static function createMagicSpace($name, $content, $toasterData, $params = array()) {
-		$name = ucfirst(strtolower($name));
-		if(self::_validate($name)) {
-			$magicSpaceClassName = 'MagicSpaces_' . $name . '_' . $name;
-			return new $magicSpaceClassName($name, $content, $toasterData, $params);
-		}
-		throw new Exceptions_SeotoasterException('Cannot run the magic space: ' . $name);
-	}
-
-	private static function _validate($name) {
-		$incPath = explode(PATH_SEPARATOR, get_include_path());
-		if(is_array($incPath) && !empty($incPath)) {
-			foreach($incPath as $path) {
-				$magicSpacesDir = $path.DIRECTORY_SEPARATOR.'MagicSpaces'.DIRECTORY_SEPARATOR.$name;
-				if (is_dir($magicSpacesDir) && file_exists($magicSpacesDir.DIRECTORY_SEPARATOR.$name.'.php')) {
-					return true;
-				}
-			}
-			return false;
-		}
-		return false;
-	}
+    private static function _validate($name)
+    {
+        $incPath = explode(PATH_SEPARATOR, get_include_path());
+        if (is_array($incPath) && !empty($incPath)) {
+            foreach ($incPath as $path) {
+                $magicSpacesDir = $path.DIRECTORY_SEPARATOR.'MagicSpaces'.DIRECTORY_SEPARATOR.$name;
+                if (is_dir($magicSpacesDir) && file_exists($magicSpacesDir.DIRECTORY_SEPARATOR.$name.'.php')) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
 }
