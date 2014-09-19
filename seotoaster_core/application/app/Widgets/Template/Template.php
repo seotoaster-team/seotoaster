89f74319809353054bf7068a56f40caa699c3e6e
PavelCSS <yashin.ziby@gmail.com>
2014-09-19 15:25:37 +0300

    Add check on template type

--- seotoaster_core/application/app/Widgets/Template/Template.php
+++ seotoaster_core/application/app/Widgets/Template/Template.php
@@ -1,10 +1,21 @@
 <?php
 
-class Widgets_Template_Template extends Widgets_Abstract {
+class Widgets_Template_Template extends Widgets_Abstract
+{
+    const TEMPLATE_TYPE = 'type_partial_template';
 
-	protected function  _load() {
-		$templateName   = array_shift($this->_options);
+    protected function  _load()
+    {
+        $templateName = array_shift($this->_options);
         $template = Application_Model_Mappers_TemplateMapper::getInstance()->find($templateName);
-        return ($template !== null) ? $template->getContent() : '';
-	}
+        if ($template !== null) {
+            if($template->getType() === self::TEMPLATE_TYPE) {
+                return $template->getContent();
+            } else {
+                return '<span style="color: red;">Choose \'Partial template\' type</span>';
+            }
+        } else {
+            return '<span style="color: red;">No template with name "' . $templateName . '"</span>';
+        }
+    }
 }
