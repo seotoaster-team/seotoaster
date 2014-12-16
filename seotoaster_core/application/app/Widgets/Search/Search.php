18d81d062c6c9257ecab785bcf5f99bfe5070f78
rodman <rodmanqs@gmail.com>
2014-12-16 19:27:32 +0200

    Add search by phrase

--- seotoaster_core/application/app/Widgets/Search/Search.php
+++ seotoaster_core/application/app/Widgets/Search/Search.php
@@ -180,9 +180,9 @@ class Widgets_Search_Search extends Widgets_Abstract
                 try {
                     if (in_array(self::OPTION_SORT_RECENT, $this->_options)
                         && array_key_exists('modified', $toasterSearchIndex->getFieldNames())) {
-                        $hits = $toasterSearchIndex->find(Zend_Search_Lucene_Search_QueryParser::parse($searchTerm,'utf-8'), 'modified', SORT_DESC);
+                        $hits = $toasterSearchIndex->find(Zend_Search_Lucene_Search_QueryParser::parse('"'.$searchTerm.'"','utf-8'), 'modified', SORT_DESC);
                     } else {
-                        $hits = $toasterSearchIndex->find(Zend_Search_Lucene_Search_QueryParser::parse($searchTerm,'utf-8'));
+                        $hits = $toasterSearchIndex->find(Zend_Search_Lucene_Search_QueryParser::parse('"'.$searchTerm.'"','utf-8'));
                     }
                 } catch (Exception $e) {
                     throw new Exceptions_SeotoasterWidgetException($e->getMessage());
