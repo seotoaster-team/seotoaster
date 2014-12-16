ef3aae693301518eed559a8a85985508f55e7280
zhenya <zhenyaaynehz@gmail.com>
2014-12-16 13:09:43 +0200

    Reformat code

--- seotoaster_core/application/app/Tools/MagicSpaces/Abstract.php
+++ seotoaster_core/application/app/Tools/MagicSpaces/Abstract.php
@@ -1,32 +1,32 @@
 <?php
 
-abstract class Tools_MagicSpaces_Abstract {
+abstract class Tools_MagicSpaces_Abstract
+{
+    protected $_name         = '';
 
-	protected $_name         = '';
+    /**
+     * Full and parsed page content
+     *
+     * @var string
+     */
+    protected $_content      = '';
 
-	/**
-	 * Full and parsed page content
-	 *
-	 * @var string
-	 */
-	protected $_content      = '';
+    /**
+     * Parsed magic space content
+     *
+     * @var string
+     */
+    protected $_spaceContent = '';
 
-	/**
-	 * Parsed magic space content
-	 *
-	 * @var string
-	 */
-	protected $_spaceContent = '';
-
-	/**
-	 * Seotoaster's current page data
-	 *
-	 * @var array
-	 */
-	protected $_toasterData  = array();
+    /**
+     * Seotoaster's current page data
+     *
+     * @var array
+     */
+    protected $_toasterData  = array();
 
 
-    protected $_magicLabel = false;
+    protected $_magicLabel   = false;
 
     /**
      * Magic space parameters. Available since 2.0.6
@@ -42,51 +42,68 @@ abstract class Tools_MagicSpaces_Abstract {
      */
     protected $_replaceKey   = '';
 
-	public function __construct($name = '', $content = '', $toasterData = array(), $params = array(), $magicLabel = false) {
-		$this->_name         = $name;
-		$this->_content      = $content;
-		$this->_toasterData  = $toasterData;
+    public function __construct(
+        $name = '',
+        $content = '',
+        $toasterData = array(),
+        $params = array(),
+        $magicLabel = false
+    ) {
+        $this->_name         = $name;
+        $this->_content      = $content;
+        $this->_toasterData  = $toasterData;
         $this->_params       = $params;
         $this->_magicLabel   = $magicLabel;
-        $this->_replaceKey   = (is_array($params) && !empty($params)) ? (':'.implode(':', $params)) : '';
-		$this->_spaceContent = $this->_parse();
-		$this->_init();
-	}
+        $this->_replaceKey   = (is_array($params) && !empty($params)) ? (':' . implode(':', $params)) : '';
+        $this->_spaceContent = $this->_parse();
+        $this->_init();
+    }
 
-	/**
-	 * This method should be overloaded in the descendant classes
-	 * If this method return's any content it will be place instead of the original magigspace content
-	 * @abstract
-	 *
-	 */
-	abstract protected function _run();
+    /**
+     * This method should be overloaded in the descendant classes
+     * If this method return's any content it will be place instead of the original magigspace content
+     *
+     * @abstract
+     */
+    abstract protected function _run();
 
-	protected function _init() {}
+    protected function _init()
+    {
+    }
 
-	public function run() {
-		$content = $this->_run();
-		return $this->_replace(($content !== null) ? $content : $this->_spaceContent);
-	}
+    public function run()
+    {
+        $content = $this->_run();
 
-	protected function _parse() {
-		if(!$this->_name) {
-			return '';
-		}
-        if($this->_magicLabel){
-            $space = Tools_Content_Parser::MAGIC_SPACE_LABEL.Tools_Content_Parser::OPTIONS_SEPARATOR.strtolower($this->_name);
-        }else{
-		    $space = strtolower($this->_name);
+        return $this->_replace(($content !== null) ? $content : $this->_spaceContent);
+    }
+
+    protected function _parse()
+    {
+        if (!$this->_name) {
+            return '';
+        }
+
+        if ($this->_magicLabel) {
+            $space = Tools_Content_Parser::MAGIC_SPACE_LABEL.Tools_Content_Parser::OPTIONS_SEPARATOR
+                .strtolower($this->_name);
+        }
+        else {
+            $space = strtolower($this->_name);
         }
         // Put parameter back into a string for valid parsing
-	    preg_match('~{'.$space.$this->_replaceKey.'}(.*){/'.$space.'}~suiU', $this->_content, $found);
+        preg_match('~{'.$space.$this->_replaceKey.'}(.*){/'.$space.'}~suiU', $this->_content, $found);
 
-		return (is_array($found) && !empty($found) && isset($found[1])) ? $found[1] : '';
-	}
+        return (is_array($found) && !empty($found) && isset($found[1])) ? $found[1] : '';
+    }
 
-	protected function _replace($spaceContent) {
-        if($this->_magicLabel){
-            $space = Tools_Content_Parser::MAGIC_SPACE_LABEL.Tools_Content_Parser::OPTIONS_SEPARATOR.strtolower($this->_name);
-        }else{
+    protected function _replace($spaceContent)
+    {
+        if ($this->_magicLabel) {
+            $space = Tools_Content_Parser::MAGIC_SPACE_LABEL.Tools_Content_Parser::OPTIONS_SEPARATOR
+                .strtolower($this->_name);
+        }
+        else {
             $space = strtolower($this->_name);
         }
         // Put parameter back for replacement
@@ -97,12 +114,14 @@ abstract class Tools_MagicSpaces_Abstract {
             1
         );
 
-	}
+    }
 
-    private function _escapeChars($content) {
+    private function _escapeChars($content)
+    {
         $chars = array(
             '$' => '\$'
         );
+        
         return str_replace(array_keys($chars), array_values($chars), $content);
     }
 }
