565b31ab204ea6d6b17094d9e15de1d0ab04bfaf
Artem <kovaloff1@gmail.com>
2014-12-22 16:31:26 +0200

    Fix duplicate content in widget code

--- seotoaster_core/application/app/Widgets/Code/Code.php
+++ seotoaster_core/application/app/Widgets/Code/Code.php
@@ -27,7 +27,8 @@ class Widgets_Code_Code extends Widgets_AbstractContent {
 		if(!preg_match('~<script~', $codeContent)) {
 			ob_start();
 			$returned    = eval($codeContent);
-			$codeContent = ob_get_flush();
+			$codeContent = ob_get_clean();
+            ob_get_flush();
 			$codeContent .= $returned;
 		}
         if(Tools_Security_Acl::isAllowed($this)) {
