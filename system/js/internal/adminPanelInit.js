3bb77741cad8533b5438b69c90b4f9fd887a14f8
Your Name <you@example.com>
2015-08-25 16:59:53 +0300

    add polyfill; re-organize js

--- system/js/internal/adminPanelInit.js
+++ system/js/internal/adminPanelInit.js
@@ -1,109 +1,199 @@
-$(function () {
+(function () {
+    'use strict';
+
     // for IE8
-    if(!document.getElementsByClassName) {
-        document.getElementsByClassName = function(className) {
+    if (!document.getElementsByClassName) {
+        document.getElementsByClassName = function (className) {
             return this.querySelectorAll("." + className);
         };
         Element.prototype.getElementsByClassName = document.getElementsByClassName;
     }
 
-    var panel = document.getElementsByClassName('seotoaster-panel')[0],
-        $sectionList = $(document.getElementsByClassName('section-list')[0]),
-        section, sectionIndx;
+    var
+        elActiveClassName = 'active',
+        websiteUrl = document.getElementById('website_url').value,
+        toasterPanel = document.getElementsByClassName('seotoaster-panel')[0],
+        sectionList = document.getElementsByClassName('section-list')[0],
+        showHideButton = document.getElementsByClassName('show-hide')[0];
 
-    if (localStorage.getItem('panel-section-active') != null) {
-        $sectionList.find('.section:eq(' + localStorage.getItem('panel-section-active') + ')').addClass('active');
+    /**
+     * Find and create all path to the event element
+     * @param element
+     * @returns {Array}
+     * @private
+     */
+    function _getPathToEvent(element) {
+        var elementPath = [];
+
+        while (element) {
+            elementPath.push(element);
+            element = element.parentNode;
+        }
+        return elementPath;
     }
 
-    $sectionList.on('click', '.section-btn:not(#cleancache)', function () {
-        section = this.parentNode;
-        sectionIndx = $(section).index();
-        if ($(section).hasClass('active')) {
-            $(section).removeClass('active');
-            localStorage.removeItem('panel-section-active');
-        } else {
-            $('.section').removeClass('active');
-            localStorage.setItem('panel-section-active', sectionIndx);
-            $(section).addClass('active');
-        }
-    });
-
-    // hide and show aside panel
-    $('.show-hide').on('click', function () {
-        $(panel).toggleClass('s h');
-        $(this).toggleClass('_i-panel-hide _i-panel-show');
-    });
-
-
-    // close section of panel when we click without panel
-    $(document).on('click', function (event) {
-        if (!$(panel).is(event.target) && $(panel).has(event.target).length === 0) {
-            $('.section').removeClass('active');
+    /**
+     *
+     * @param element
+     * @private
+     */
+    function _removeActiveClass(element) {
+        if (element != null) {
+            element.classList.remove(elActiveClassName);
             localStorage.removeItem('panel-section-active');
         }
-    });
+    }
 
-    // get CMS version for the notices box
-    $.get($('#website_url').val() + '/backend/backend_update/version/', function (response) {
-        if (!response.error && response.responseText.status == 1) {
-            $('._i-notifications').addClass('new-notices');
-            $('.notice._new-version').addClass('show');
+    /**
+     *
+     * @param element
+     * @returns {number}
+     * @private
+     */
+    function _getItemIndex(element) {
+        var list = _getItems();
+        for (var i = 0; i < list.length; i++) {
+            if (list[i] == element) {
+                return i;
+            }
         }
-    });
+    }
 
-    //admin panel edit 404 page click
-    $('#edit404').click(function () {
-        $.get($('#website_url').val() + '/backend/backend_page/edit404page', function (responseText) {
-            console.log(responseText);
-            if (responseText.notFoundUrl) {
-                window.location.href = responseText.notFoundUrl;
+    /**
+     *
+     * @returns {Array}
+     * @private
+     */
+    function _getItems() {
+        var
+            list = document.getElementsByClassName('section-list')[0].childNodes,
+            result = [];
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].nodeType == 1) {
+                result.push(list[i]);
             }
-            else {
-                smoke.alert($('.edit404-msg').html(), {'classname': 'errors'});
-            }
-        });
-    });
+        }
+        return result;
+    }
 
-    // Clean all cache
-    $('#cleancache').on('click', function () {
-        var $this = $(this);
-        $this.removeClass('ticon-loop');
-        $this.addClass('run ticon-loading');
-        //showMessage('Clearing cache...', false);
+    if (localStorage.getItem('panel-section-active') != null) {
+        var list = _getItems();
+        list[localStorage.getItem('panel-section-active')].classList.add('active');
+    }
 
-        $.get($('#website_url').val() + 'backend/backend_content/cleancache/', function (response) {
-            if (response.error == 0) {
-                showMessage(response.responseText, false, 2500);
-                $this.removeClass('run ticon-loading').addClass('ticon-loop');
-            }
-            else {
-                showMessage(response.responseText, true);
-                $this.removeClass('run ticon-loading').addClass('ticon-loop');
-            }
-        });
-    });
-
-    //admin panel delete this page link
-    $('#del-this-page').click(function () {
-        var pageId = $('#del-page-id').val();
-        var websiteUrl = $('#website_url').val();
-
-        var isCategory = !!($(this).data('cid') == 0);
-        if (isCategory) {
-            $.getJSON(websiteUrl + 'backend/backend_page/checkforsubpages/pid/' + pageId, function (response) {
-                if (response.responseText.subpages) {
-                    smoke.alert(response.responseText.message, function (e) {
-                    }, {'classname': 'warning'});
-                    return false;
-                } else {
-                    showDelConfirm();
+    /**
+     * Get CMS version
+     * @private
+     */
+    function _getCMSVersion(){
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", websiteUrl + '/backend/backend_update/version/', true);
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                var response = JSON.parse(xmlHttp.responseText);
+                if (response.status == 1) {
+                    document.getElementsByClassName('_i-notification')[0].classList.add('new-notices');
+                    document.getElementsByClassName('_new-version')[0].classList.add('show');
                 }
-            });
-        } else {
-            showDelConfirm();
+            }
+        };
+        xmlHttp.send(null);
+    }
+
+
+    window.addEventListener('load', function(){
+        _getCMSVersion();
+    });
+
+
+    /**
+     * Event on the panel item
+     */
+    sectionList.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        var item = event.target.parentNode,
+            itemWithClass = sectionList.querySelector('.section.active');
+
+        if (event.target.className.indexOf('section-btn') == -1 || event.target.className.indexOf('no-open-section') != -1) {
+            return false;
         }
-    })
-});
+        // if haven't class
+        if (!item.classList.contains(elActiveClassName)) {
+            _removeActiveClass(itemWithClass);
+            localStorage.setItem('panel-section-active', _getItemIndex(item));
+            item.classList.add(elActiveClassName);
+        } else {
+            _removeActiveClass(item);
+        }
+    });
+
+    /**
+     * Close the section if we click outside the panel
+     */
+    document.addEventListener('click', function (event) {
+        var path = _getPathToEvent(event.target);
+
+        // We use '-2' because 'window' and 'document' cannot have class
+        for (var i = 0; i < path.length - 2; i++) {
+            if (path[i].classList.contains('seotoaster-panel')) {
+                return false;
+            }
+        }
+        _removeActiveClass(sectionList.querySelector('.section.active'));
+    });
+
+    document.getElementsByClassName('show-hide')[0].addEventListener('click', function (event) {
+        event.target.classList.toggle('_i-panel-hide');
+        event.target.classList.toggle('_i-panel-show');
+        toasterPanel.classList.toggle('s');
+        toasterPanel.classList.toggle('h');
+    });
+
+    /**
+     * Delete current page
+     */
+    var elDel = document.getElementById('del-this-page');
+    if (elDel)
+        elDel.addEventListener('click', function (event) {
+            var pId = document.getElementById('del-page-id').value, cId = event.target.getAttribute('data-cid');
+
+            if (cId == 0) {
+                var xmlHttp = new XMLHttpRequest();
+                xmlHttp.open("GET", websiteUrl + 'backend/backend_page/checkforsubpages/pid/' + pId, false);
+                xmlHttp.send(null);
+                if (xmlHttp.status == 200) {
+                    if (xmlHttp.responseText.subpages) {
+                        smoke.alert(xmlHttp.responseText.message, function (e) {
+                        }, {'classname': 'warning'});
+                        return false;
+                    } else {
+                        showDelConfirm();
+                    }
+                }
+            } else {
+                showDelConfirm();
+            }
+        });
+
+    /**
+     * Check 404 page
+     */
+    document.getElementById('edit404').addEventListener('click', function () {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", websiteUrl + '/backend/backend_page/edit404page', false);
+        xmlHttp.send(null);
+        if (xmlHttp.status == 200) {
+            var response = JSON.parse(xmlHttp.responseText);
+            if (response.notFoundUrl) {
+                window.location.href = response.notFoundUrl;
+            }
+            else {
+                smoke.alert(document.getElementsByClassName('edit404-msg')[0].innerHTML, {'classname': 'errors'});
+            }
+        }
+    });
+})();
 
 function showDelConfirm() {
     var pageId = $('#del-page-id').val();
@@ -111,13 +201,13 @@ function showDelConfirm() {
     smoke.confirm('Are you sure you want to delete this page?', function (e) {
         if (e) {
             $.ajax({
-                url: websiteUrl + 'backend/backend_page/delete/' + 'id/' + pageId,
-                type: 'DELETE',
-                dataType: 'json',
+                url       : websiteUrl + 'backend/backend_page/delete/' + 'id/' + pageId,
+                type      : 'DELETE',
+                dataType  : 'json',
                 beforeSend: function () {
                     smoke.signal('Removing page...', 30000);
                 },
-                success: function (response) {
+                success   : function (response) {
                     hideSpinner();
                     if (!response.error) {
                         top.location.href = websiteUrl;
