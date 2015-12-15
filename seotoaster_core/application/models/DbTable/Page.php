c76975e5c8450d230d010946958b2019a0b98fb1
Vitaly-V <gtopbox@gmail.com>
2015-12-15 13:48:19 +0000

    Add page_type to the find method

--- seotoaster_core/application/models/DbTable/Page.php
+++ seotoaster_core/application/models/DbTable/Page.php
@@ -160,7 +160,7 @@ class Application_Model_DbTable_Page extends Zend_Db_Table_Abstract {
 
     private function _getOptimizedSelect($originalsOnly, $pageFields = array()) {
         if(empty($pageFields)) {
-            $pageFields = array('id', 'template_id', 'parent_id', 'last_update', 'is_404page', 'show_in_menu', 'order', 'weight', 'silo_id', 'protected', 'system', 'draft', 'publish_at', 'news', 'err_login_landing', 'mem_landing', 'signup_landing', 'preview_image', 'external_link_status', 'external_link');
+            $pageFields = array('id', 'template_id', 'parent_id', 'last_update', 'is_404page', 'show_in_menu', 'order', 'weight', 'silo_id', 'protected', 'system', 'draft', 'publish_at', 'news', 'err_login_landing', 'mem_landing', 'signup_landing', 'preview_image', 'external_link_status', 'external_link', 'page_type');
         }
         $select = $this->getAdapter()->select();
         if($originalsOnly) {
