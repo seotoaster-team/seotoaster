839b2786fa3c8f15a5adb4a1baed4271daaf4768
Vitaly-V <gtopbox@gmail.com>
2015-12-17 13:07:43 +0000

    Fix for direct upload

--- seotoaster_core/application/controllers/Backend/UploadController.php
+++ seotoaster_core/application/controllers/Backend/UploadController.php
@@ -267,7 +267,7 @@ class Backend_UploadController extends Zend_Controller_Action
                     ->addFilter(new Zend_Filter_PregReplace(array('match' => '/[^\w\d_]+/u', 'replace' => '-')));
 
                 // filtering the img name
-                $expFileName = explode('.', $this->_uploadHandler->getFileName(null, false));
+                $expFileName = explode('.', $this->getRequest()->getParam('name', false));
                 $fileExt = array_pop($expFileName);
                 $name = implode($expFileName);
                 $newName = $filterChain->filter($name) . '.' . $fileExt;
