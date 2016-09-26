bc0070b18b67c28f204cdcbfdd667b26c81c2e43
Vitaly-V <gtopbox@gmail.com>
2016-09-26 13:47:00 +0300

    Refacrot iCalendar tool to static

--- seotoaster_core/application/app/Tools/System/ICalendar.php
+++ seotoaster_core/application/app/Tools/System/ICalendar.php
@@ -2,75 +2,46 @@
 
 class Tools_System_ICalendar {
 
-    private $uid;
-
-    private $start;
-
-    private $end;
-
-    private $summary;
-
-    private $description;
-
-    private $location;
-
-    protected $title;
-
-    protected $author;
-
-    public function __construct($parameters) {
-        $parameters += array(
-            'summary' => 'Untitled Event',
-            'description' => '',
-            'location' => '',
-            'events' => array(),
-            'title' => 'Calendar',
-            'author' => 'Calender Generator'
-        );
-
-        if (isset($parameters['uid'])) {
-            $this->uid = $parameters['uid'];
-        } else {
-            $this->uid = uniqid(rand(0, getmypid()));
-        }
-        $this->start = $parameters['start'];
-        $this->end = $parameters['end'];
-        $this->summary = $parameters['summary'];
-        $this->description = $parameters['description'];
-        $this->location = $parameters['location'];
-        $this->title  = $parameters['title'];
-        $this->author = $parameters['author'];
-    }
-
     /**
      * Get the start time set for the even
      * @return string
      */
-    private function formatDate($date) {
+    public static function formatDate($date) {
         $date = new DateTime($date);
         return $date->format("Ymd\THis\Z");
     }
 
-    private function formatValue($str) {
+    public static function formatValue($str) {
         return addcslashes($str, ",\\;");
     }
 
-    public function generateString() {
+    public static function generateString($parameters) {
+        $parameters += array(
+            'summary' => 'Untitled Event',
+            'description' => '',
+            'location' => '',
+            'title' => 'Calendar',
+            'author' => 'Calender Generator'
+        );
+        $parameters = filter_var_array($parameters);
+        if (empty($parameters['uid'])) {
+            $parameters['uid'] = uniqid(rand(0, getmypid()));
+        }
         return "BEGIN:VCALENDAR\r\n"
             . "VERSION:2.0\r\n"
-            . "PRODID:-//" . $this->author . "//NONSGML//EN\r\n"
-            . "X-WR-CALNAME:" . $this->title . "\r\n"
+            . "PRODID:-//" . $parameters['author ']. "//NONSGML//EN\r\n"
+            . "X-WR-CALNAME:" . $parameters['title ']. "\r\n"
             . "CALSCALE:GREGORIAN\r\n"
             . "BEGIN:VEVENT\r\n"
-            . "UID:{$this->uid}\r\n"
-            . "DTSTART:{$this->formatDate($this->start)}\r\n"
-            . "DTEND:{$this->formatDate($this->end)}\r\n"
-            . "DTSTAMP:{$this->formatDate($this->start)}\r\n"
-            . "CREATED:{$this->formatDate(date('Y/m/d H:i:s'))}\r\n"
-            . "DESCRIPTION:{$this->formatValue($this->description)}\r\n"
-            . "LAST-MODIFIED:{$this->formatDate($this->start)}\r\n"
-            . "LOCATION:{$this->location}\r\n"
-            . "SUMMARY:{$this->formatValue($this->summary)}\r\n"
+            . "UID:{$parameters['uid']}\r\n"
+            . "DTSTART:{" . self::formatDate($parameters['start']) . "}\r\n"
+            . "DTEND:{" . self::formatDate($parameters['end']) . "}\r\n"
+            . "DTSTAMP:{" . self::formatDate($parameters['start']) . "}\r\n"
+            . "CREATED:{" . self::formatDate(date('Y/m/d H:i:s')) . "}\r\n"
+            . "DESCRIPTION:{" . self::formatValue($parameters['description']) . "}\r\n"
+            . "LAST-MODIFIED:{" . self::formatDate($parameters['start']) . "}\r\n"
+            . "LOCATION:{$parameters['location']}\r\n"
+            . "SUMMARY:{" . self::formatValue($parameters['summary']) . "}\r\n"
             . "SEQUENCE:0\r\n"
             . "STATUS:CONFIRMED\r\n"
             . "TRANSP:OPAQUE\r\n"
@@ -82,9 +53,9 @@ class Tools_System_ICalendar {
      *
      * Call this function to download the invite.
      */
-    public function generateDownloadInvite()
+    public static function generateDownloadInvite()
     {
-        $generated = $this->generateString();
+        $generated = self::generateString();
         header('Expires: Sat, 26 Jul 1997 05:00:00 GMT' ); //date in the past
         header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' ); //tell it we just updated
         header('Cache-Control: no-store, no-cache, must-revalidate' ); //force revaidation
@@ -103,15 +74,17 @@ class Tools_System_ICalendar {
      *
      * Call this function to email the invite.
      */
-    public function generateEmailInviteAttachment()
+    public static function generateEmailInviteAttachment($parameters)
     {
-        $generated = $this->generateString();
+        $type = !empty($parameters['mimeType']) ? $parameters['mimeType'] : 'text/calendar';
+        $fileName = !empty($parameters['mimeType']) ? $parameters['fileName'] : 'calendar.ics';
+        $generated = self::generateString($parameters);
         $at              = new Zend_Mime_Part($generated);
-        $at->type        = 'text/calendar';
+        $at->type        = $type;
         $at->disposition = Zend_Mime::DISPOSITION_ATTACHMENT;
         $at->encoding    = Zend_Mime::ENCODING_8BIT;
-        $at->filename    = 'calendar.ics';
+        $at->filename    = $fileName;
         return $at;
-
     }
+
 }
