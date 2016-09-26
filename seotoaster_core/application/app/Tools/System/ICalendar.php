9c32130de2b0a79cd6c09b04dbb540db0a89340c
Vitaly-V <gtopbox@gmail.com>
2016-09-26 16:46:29 +0300

    Improve code for ICalendar tool

--- seotoaster_core/application/app/Tools/System/ICalendar.php
+++ seotoaster_core/application/app/Tools/System/ICalendar.php
@@ -1,21 +1,25 @@
 <?php
 
-class Tools_System_ICalendar {
+class Tools_System_ICalendar
+{
 
     /**
      * Get the start time set for the even
      * @return string
      */
-    public static function formatDate($date) {
+    public static function formatDate($date)
+    {
         $date = new DateTime($date);
         return $date->format("Ymd\THis\Z");
     }
 
-    public static function formatValue($str) {
+    public static function formatValue($str)
+    {
         return addcslashes($str, ",\\;");
     }
 
-    public static function generateString($parameters) {
+    public static function generateString($parameters)
+    {
         $parameters += array(
             'summary' => 'Untitled Event',
             'description' => '',
@@ -27,46 +31,44 @@ class Tools_System_ICalendar {
         if (empty($parameters['uid'])) {
             $parameters['uid'] = uniqid(rand(0, getmypid()));
         }
-        return "BEGIN:VCALENDAR\r\n"
-            . "VERSION:2.0\r\n"
-            . "PRODID:-//" . $parameters['author ']. "//NONSGML//EN\r\n"
-            . "X-WR-CALNAME:" . $parameters['title ']. "\r\n"
-            . "CALSCALE:GREGORIAN\r\n"
-            . "BEGIN:VEVENT\r\n"
-            . "UID:{$parameters['uid']}\r\n"
-            . "DTSTART:{" . self::formatDate($parameters['start']) . "}\r\n"
-            . "DTEND:{" . self::formatDate($parameters['end']) . "}\r\n"
-            . "DTSTAMP:{" . self::formatDate($parameters['start']) . "}\r\n"
-            . "CREATED:{" . self::formatDate(date('Y/m/d H:i:s')) . "}\r\n"
-            . "DESCRIPTION:{" . self::formatValue($parameters['description']) . "}\r\n"
-            . "LAST-MODIFIED:{" . self::formatDate($parameters['start']) . "}\r\n"
-            . "LOCATION:{$parameters['location']}\r\n"
-            . "SUMMARY:{" . self::formatValue($parameters['summary']) . "}\r\n"
-            . "SEQUENCE:0\r\n"
-            . "STATUS:CONFIRMED\r\n"
-            . "TRANSP:OPAQUE\r\n"
-            . "END:VEVENT\r\n"
-            . "END:VCALENDAR";
+        return "BEGIN:VCALENDAR" . PHP_EOL
+        . "VERSION:2.0" . PHP_EOL
+        . "PRODID:-//" . $parameters['author'] . "//NONSGML//EN" . PHP_EOL
+        . "X-WR-CALNAME:" . $parameters['title'] . PHP_EOL
+        . "CALSCALE:GREGORIAN" . PHP_EOL
+        . "BEGIN:VEVENT" . PHP_EOL
+        . "UID:" . $parameters['uid'] . PHP_EOL
+        . "DTSTART:" . self::formatDate($parameters['start']) . PHP_EOL
+        . "DTEND:" . self::formatDate($parameters['end']) . PHP_EOL
+        . "DTSTAMP:" . self::formatDate($parameters['start']) . PHP_EOL
+        . "CREATED:" . self::formatDate(date('Y/m/d H:i:s')) . PHP_EOL
+        . "DESCRIPTION:" . self::formatValue($parameters['description']) . PHP_EOL
+        . "LAST-MODIFIED:" . self::formatDate($parameters['start']) . PHP_EOL
+        . "LOCATION:" . $parameters['location'] . PHP_EOL
+        . "SUMMARY:" . self::formatValue($parameters['summary']) . PHP_EOL
+        . "SEQUENCE:0" . PHP_EOL
+        . "STATUS:CONFIRMED" . PHP_EOL
+        . "TRANSP:OPAQUE" . PHP_EOL
+        . "END:VEVENT" . PHP_EOL
+        . "END:VCALENDAR";
     }
 
     /**
      *
      * Call this function to download the invite.
      */
-    public static function generateDownloadInvite()
+    public static function generateDownloadInvite($params)
     {
-        $generated = self::generateString();
-        header('Expires: Sat, 26 Jul 1997 05:00:00 GMT' ); //date in the past
-        header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' ); //tell it we just updated
-        header('Cache-Control: no-store, no-cache, must-revalidate' ); //force revaidation
-        header('Cache-Control: post-check=0, pre-check=0', false );
-        header('Pragma: no-cache' );
-        header('Content-type: text/calendar; charset=utf-8');
-        header('Content-Disposition: inline; filename="calendar.ics"');
-        header("Content-Description: File Transfer");
-        header("Content-Transfer-Encoding: binary");
-        header("Content-Length: " . strlen($generated));
-        print $generated;
+        $generated = self::generateString($params);
+        $response = Zend_Controller_Front::getInstance()->getResponse();
+        $response->setHeader(
+            'Content-Disposition',
+            'attachment; filename=calendar.ics',
+            'Content-type', 'application/force-download',
+            'Content-Length', strlen($generated)
+        );
+        $response->setBody($generated);
+        $response->sendResponse();
         exit;
     }
 
@@ -76,14 +78,16 @@ class Tools_System_ICalendar {
      */
     public static function generateEmailInviteAttachment($parameters)
     {
-        $type = !empty($parameters['mimeType']) ? $parameters['mimeType'] : 'text/calendar';
-        $fileName = !empty($parameters['mimeType']) ? $parameters['fileName'] : 'calendar.ics';
+        $type = !empty($parameters['mimeType']) ? filter_var($parameters['mimeType'],
+            FILTER_SANITIZE_STRING) : 'text/calendar';
+        $fileName = !empty($parameters['mimeType']) ? filter_var($parameters['fileName'],
+            FILTER_SANITIZE_STRING) : 'calendar.ics';
         $generated = self::generateString($parameters);
-        $at              = new Zend_Mime_Part($generated);
-        $at->type        = $type;
+        $at = new Zend_Mime_Part($generated);
+        $at->type = $type;
         $at->disposition = Zend_Mime::DISPOSITION_ATTACHMENT;
-        $at->encoding    = Zend_Mime::ENCODING_8BIT;
-        $at->filename    = $fileName;
+        $at->encoding = Zend_Mime::ENCODING_8BIT;
+        $at->filename = $fileName;
         return $at;
     }
 
