c6fde480d64910519be6113373f91ffa83de035d
Vorobyov Valentin <vorobyovvalentin@gmail.com>
2017-11-10 12:00:05 +0200

    rework media uploader screen

--- seotoaster_core/application/controllers/Backend/MediaController.php
+++ seotoaster_core/application/controllers/Backend/MediaController.php
@@ -41,7 +41,20 @@ class Backend_MediaController extends Zend_Controller_Action
         // if folder selected from somewhere else (using this feature when click upload things on editor screen)
         if (($folder = $this->getRequest()->getParam('folder')) != '') {
             $this->view->currFolder = $folder;
+            $picturesPath = $this->_websiteConfig['media'] . $folder . '/small/';
+            if(is_dir($this->_websiteConfig['path'] . $picturesPath)){
+                $listPictures = Tools_Filesystem_Tools::scanDirectory($this->_websiteConfig['path'] . $picturesPath);
+
+                if(!empty($listPictures)){
+                    $this->view->listPictures = $listPictures;
+                    $this->view->picturesPath = $picturesPath;
+                }
+            }
         }
+
+        $secureToken = Tools_System_Tools::initSecureToken(Tools_System_Tools::ACTION_PREFIX_REMOVETHINGS);
+        $this->view->secureToken = $secureToken;
+
         $this->view->helpSection = 'uploadthings';
     }
 
