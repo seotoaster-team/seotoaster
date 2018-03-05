144af490cfefef69430d7b519ee17b7d6b89abe8
Vitaly-V <gtopbox@gmail.com>
2018-03-05 15:42:08 +0200

    Caching pwa-offline page

--- sw-base.js
+++ sw-base.js
@@ -1,34 +1,37 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js');
 const STATIC_FILES = [
   '/',
-  '/index.html',
   '/pwa-offline.html',
 ];
 
 if (workbox) {
-  workbox.routing.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/,
-    workbox.strategies.staleWhileRevalidate({
-      cacheName: 'google-сache'
-    })
-  );
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('static').then(cache => {
+        cache.addAll(STATIC_FILES);
+      })
+    );
+  });
+
 
   workbox.routing.registerRoute(
     routeData => routeData.event.request.headers.get('accept').includes('text/html'),
-    args => {
-      return caches.match(args.event.request).then(response => {
-      if (response) {
-        return response;
-      } else {
-        return fetch(args.event.request)
-          .then(res => caches.open(STATIC_FILES).then(cache => cache.addAll(STATIC_FILES)))
-          .catch(err => caches.match('/pwa-offline.html').then(res => res));
-      }
-    })
-  });
+    args => fetch(args.event.request)
+      .then(res => res)
+      .catch(err =>
+        {
+          console.log("IN ERROR", err);
+          return caches.match('/pwa-offline.html')
+            .then(res =>  {
+                console.log("IN CACHE", err);
+               return res;
+              }
+            )
+        }
+      )
+  );
 
   workbox.precaching.precacheAndRoute([]);
-
-
   
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,21 @@
 module.exports = {
   "globDirectory": "./",
   "globPatterns": [
-    "**/*.{png,jpg,css,html,json,txt,js,gif,jpeg,rtf,ttf,svg,eot,woff}"
+    "system/css/**/*.css",
+    "system/js/**/*.min.js",
+    "plugins/widcard/*.{png,jpg,css,html,min.js,gif,jpeg}",
+    "themes/**/*.min.js",
+    "themes/**/concat_*.css"
   ],
   "swDest": "sw.js",
   "swSrc": "./sw-base.js",
   "globIgnores": [
     "_install/**",
+    "feeds/**",
+    "cache/**",
+    "tmp/**",
+    "tests/**",
+    "system/images/flags/**",
+    "system/js/external/tinymce/**",
   ]
 };
