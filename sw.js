46c82bef55cee9cc507be60df472b8b475b3ea2c
Vitaly-V <gtopbox@gmail.com>
2018-03-21 13:19:54 +0200

    Add revision to SW

--- sw.js
+++ sw.js
@@ -1,15 +1,8 @@
+const revision = 'pwa03202018';
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 //# sourceMappingURL=workbox-sw.js.map
-const STATIC_FILES = [
-  '/',
-  '/pwa-offline.html',
-  '/tmp/offline.concat.min.css'
-];
 
 if (workbox) {
-
-  workbox.skipWaiting();
-  workbox.clientsClaim();
 
   workbox.routing.registerRoute(
     routeData => routeData.event.request.headers.get('accept').includes('text/html'),
@@ -23,18 +16,18 @@ if (workbox) {
     workbox.strategies.staleWhileRevalidate({cacheName: 'fonts'})
   );
 
-  workbox.routing.registerRoute(/plugins\/widcard\/system\/userdata\/.*\.(png|jpg)$/,
+  workbox.routing.registerRoute(/\/plugins\/widcard\/system\/userdata\/.*\.(png|jpg)$/,
     workbox.strategies.staleWhileRevalidate({cacheName: 'images'})
   );
 
   workbox.precaching.precacheAndRoute([
     {
-      "url": "pwa-offline.html",
-      "revision": new Date().toDateString(),
+      "url": "/pwa-offline.html",
+      "revision": revision,
     },
     {
-      "url": "tmp/offline.concat.min.css",
-      "revision":  new Date().toDateString(),
+      "url": "/tmp/offline.concat.min.css",
+      "revision":  revision,
     },
   ]);
 }
