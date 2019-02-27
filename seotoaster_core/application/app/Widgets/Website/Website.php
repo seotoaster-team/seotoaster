3ad45e2e34a32206644a0cf4ba5e72231d6a5909
Valentin Vorobyov <vorobyovvalentin@gmail.com>
2019-02-27 13:10:47 +0200

    Add website host widget

--- seotoaster_core/application/app/Widgets/Website/Website.php
+++ seotoaster_core/application/app/Widgets/Website/Website.php
@@ -8,6 +8,11 @@ class Widgets_Website_Website extends Widgets_Abstract {
 
 	const OPT_URL = 'url';
 
+    /**
+     * Return website host
+     */
+    const OPT_HOST = 'host';
+
 	protected function  _load() {
 		$content = '';
 		$type    = $this->_options[0];
@@ -15,6 +20,9 @@ class Widgets_Website_Website extends Widgets_Abstract {
 			case self::OPT_URL:
 				$content = $this->_toasterOptions['websiteUrl'];
 			break;
+            case self::OPT_HOST:
+                $content = Tools_System_Tools::getUrlHost($this->_toasterOptions['websiteUrl']);
+                break;
 		}
 		return $content;
 	}
