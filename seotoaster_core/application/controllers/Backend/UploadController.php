3e8c1415ec3a0e49025225879d53460db360d052
andreybudchenko <andrei@seosamba.com>
2021-11-03 16:31:44 +0200

    Add ability to upload webp format in media folder

--- seotoaster_core/application/controllers/Backend/UploadController.php
+++ seotoaster_core/application/controllers/Backend/UploadController.php
@@ -240,11 +240,11 @@ class Backend_UploadController extends Zend_Controller_Action
         }
 
         $this->_uploadHandler->clearValidators()
-            ->addValidator('Extension', false, array('jpeg', 'jpg', 'png', 'gif'))
+            ->addValidator('Extension', false, array('jpeg', 'jpg', 'png', 'gif', 'webp'))
             ->addValidator('ImageSize', false, array('maxwidth' => $miscConfig['imgMaxWidth'], 'maxheight' => $miscConfig['imgMaxWidth']));
 
         if ($this->_checkMime) {
-            $this->_uploadHandler->addValidator(new Validators_MimeType(array('image/gif', 'image/jpeg', 'image/jpg', 'image/png')), false);
+            $this->_uploadHandler->addValidator(new Validators_MimeType(array('image/gif', 'image/jpeg', 'image/jpg', 'image/png', 'image/webp')), false);
         }
 
         $receivePath = ($resize ? $savePath . DIRECTORY_SEPARATOR . 'original' : $savePath);
@@ -271,7 +271,7 @@ class Backend_UploadController extends Zend_Controller_Action
                 $fileExt = array_pop($expFileName);
                 $fileExt = strtolower($fileExt);
 
-                if (!in_array($fileExt, array('jpeg', 'jpg', 'png', 'gif'))) {
+                if (!in_array($fileExt, array('jpeg', 'jpg', 'png', 'gif', 'webp'))) {
                     return array('error' => true, 'result' => "Wrong file extension");
                 }
                 $name = implode($expFileName);
@@ -391,6 +391,7 @@ class Backend_UploadController extends Zend_Controller_Action
             case 'image/jpg':
             case 'image/jpeg':
             case 'image/gif':
+            case 'image/webp':
                 $result = $this->_uploadImages($savePath);
                 break;
             default:
