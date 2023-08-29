4f0c809d24d03d9d66cca9d56d368cf31bfc2ad8
Valentin Vorobyov <vorobyovvalentin@gmail.com>
2023-08-29 17:05:03 +0300

    clear system

--- seotoaster_core/application/models/Mappers/PageMapper.php
+++ seotoaster_core/application/models/Mappers/PageMapper.php
@@ -540,7 +540,7 @@ class Application_Model_Mappers_PageMapper extends Application_Model_Mappers_Abs
     public function publishChildPages($parentId)
     {
         $where = $this->getDbTable()->getAdapter()->quoteInto("parent_id =?", $parentId);
-        return $this->getDbTable()->update(array('draft'=>0, '`system`'=>0), $where);
+        return $this->getDbTable()->update(array('draft'=>0, 'system'=>0), $where);
     }
 
     /**
