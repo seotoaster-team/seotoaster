ab9727889ea5ae902e06c7cabfa16b3f93b0f864
Andrey Budchenko <andrei@seosamba.com>
2026-03-12 19:18:44 +0200

    Add multi-language sorting tool

--- seotoaster_core/application/app/Tools/System/Tools.php
+++ seotoaster_core/application/app/Tools/System/Tools.php
@@ -678,7 +678,7 @@ class Tools_System_Tools {
         });
 
         if ($reverseLabels === true) {
-            asort($phoneCodes);
+            $phoneCodes = self::multiLanguageSort($phoneCodes);
         }
 
         return $phoneCodes;
@@ -1101,6 +1101,72 @@ class Tools_System_Tools {
         $locale = $translator->getLocale();
         return $locale;
 
+    }
+
+    /**
+     *
+     * Multi language sorting
+     *
+     * @param array $data data sort
+     * @param string $language
+     * @return array
+     * @throws Zend_Exception
+     */
+    public static function multiLanguageSort(array $data, $language = '')
+    {
+        $locale = Zend_Registry::get('Zend_Locale');
+        if (empty($language)) {
+            $language = $locale->getLanguage();
+        }
+
+        if (empty($language) || mb_strlen($language) > 2) {
+            $language = 'en';
+        }
+
+        if ($language === 'en') {
+            asort($data);
+            return $data;
+        }
+
+        if (class_exists('Collator')) {
+            try {
+                $region = $locale->getRegion();
+
+                if (empty($region)) {
+                    $localeCode = $language; // e.g., 'fr_FR'
+                    if (strpos($localeCode, '_') === false) {
+                        // fallback: append uppercase language code as country
+                        $localeCode .= '_' . strtoupper($language);
+                    }
+                } else {
+                    $localeCode = $language. '_' . strtoupper($region);
+                }
+
+                $collator = new Collator($localeCode); // e.g., 'fr_FR'
+                $collator->asort($data);
+                return $data;
+            } catch (Exception $e) {
+                return $data;
+            }
+        }
+
+        // Fallback: transliterate + strcmp
+        uasort($data, function ($a, $b) {
+            return strcmp(self::transliterateString($a), self::transliterateString($b));
+        });
+
+        return $data;
+    }
+
+    /**
+     * Transliterate string
+     *
+     * @param string $str
+     * @return false|string
+     */
+    public static function transliterateString($str)
+    {
+        return iconv('UTF-8', 'ASCII//TRANSLIT', $str); // é → e
     }
 
 }
