29e5d34bf5ffdb31890a7a31a27e5f657552322d
Andrey Budchenko <andrei@seosamba.com>
2026-03-24 15:16:22 +0200

    Fix installer version

--- _install/installer/resourses/seotoaster.sql
+++ _install/installer/resourses/seotoaster.sql
@@ -40,7 +40,7 @@ INSERT INTO `config` (`name`, `value`) VALUES
 ('optimizedNotifications', ''),
 ('wraplinks', '0'),
 ('takeATour', '1'),
-('version',	'3.12.2');
+('version',	'3.12.4');
 
 
 DROP TABLE IF EXISTS `container`;
